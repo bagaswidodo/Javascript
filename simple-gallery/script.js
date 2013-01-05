4c6e3bb205ed67b6af3d1bd545a8e09c12f750a1
Bagas Widodo <bagaswidodo@gmail.com>
2013-01-05 10:35:54 +0700

    Revert "Contoh Javascript"
    This reverts commit dcb39cee0c5efc931d4ef642e2615d45ea86046a.

--- simple-gallery/script.js
+++
@@ -1,7 +0,0 @@
-function gantiGambar()
-{
-	var gb = document.galleri.select.value;
-	
-	//untuk menampilkan
-	document.getElementById('album').innerHTML = "<img src=image/" + gb + " width=100 height=100>";
-}
